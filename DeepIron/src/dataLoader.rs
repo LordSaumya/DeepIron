314813608a5436cea0a64e9bf298e5cb77331663
Saumya Shah <68504323+LordSaumya@users.noreply.github.com>
2023-10-27 10:55:21 +0800

    Implement dataframe transformer

--- DeepIron/src/dataLoader.rs
+++ DeepIron/src/dataLoader.rs
@@ -1,6 +1,26 @@
 use std::path::Path;
 use polars::prelude::*;
 
+
+trait dataFrameTransformer {
+    fn transform(
+        &mut self,
+        columns: &[&usize],
+        unary_function: impl Fn(&Series) -> Series,
+    ) -> Result<()>;
+}
+
+
+impl DataFrameTransformer for DataFrame {
+    fn transform(&mut self, columns: &[&usize], unary_function: impl Fn(&Series) -> Series) -> Result<()> {
+        for (_, series) in self.get_columns() {
+            let transformed_series = unary_function(series)?;
+            self.with_column(transformed_series)?;
+        }
+        Ok(());
+    }
+}
+
 pub mod dataLoader {
     pub fn loadCSV(path: &Path) -> Result<DataFrame> {
         let path = Path::new(path);
