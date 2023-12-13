bb890d4769539eb927fb241e68e658279bc50f35
Saumya Shah <68504323+LordSaumya@users.noreply.github.com>
2023-12-13 12:12:03 +0800

    Add prediction, accuracy, and loss functions

--- DeepIron/src/logistic_regression.rs
+++ DeepIron/src/logistic_regression.rs
@@ -1,20 +1,20 @@
 use crate::data_loader::DataFrameTransformer;
-use crate::model::*;
 use crate::model::loss_functions::{LossFunction, LossFunctionType};
+use crate::model::*;
 use polars::prelude::*;
 use polars::series::Series;
 
 /// A struct that defines a logistic model.
-/// 
+///
 /// # Example
-/// 
+///
 /// ```
 /// let model = Model::Logistic::new();
-/// 
+///
 /// model.fit(&x, &y);
-/// 
+///
 /// let y_pred = model.predict(&x);
-/// 
+///
 /// ```
 pub struct Logistic {
     // Fields for training
@@ -29,9 +29,9 @@ pub struct Logistic {
 
 impl Logistic {
     /// Create a new Logistic model.
-    /// 
+    ///
     /// # Example
-    /// 
+    ///
     /// ```
     /// let model = Model::Logistic::new();
     /// ```
@@ -40,10 +40,25 @@ impl Logistic {
         Logistic {
             x: x,
             y: y,
-            loss_function: LossFunctionType::binaryCrossEntropy,
+            loss_function: LossFunctionType::BinaryCrossEntropy,
             intercept: 0.0,
             coefficients: vec![0.0; x_width],
         }
+    }
+
+    pub fn round_predictions(predictions: &Series) -> Series {
+        let mut rounded_predictions: Vec<f64> = Vec::with_capacity(predictions.len());
+
+        for prediction in predictions.f64().unwrap().into_iter() {
+            let prediction = prediction.unwrap();
+            if prediction > 0.5 {
+                rounded_predictions.push(1.0);
+            } else {
+                rounded_predictions.push(0.0);
+            }
+        }
+
+        Series::new("prediction", rounded_predictions)
     }
 
     fn compute_gradients(&self, predictions: &Series) -> (f64, Vec<f64>) {
@@ -52,7 +67,11 @@ impl Logistic {
         let intercept_gradient: f64 = error.mean().unwrap() * -2.0;
 
         for (_i, _) in self.coefficients.iter().enumerate() {
-            let gradient: f64 = self.loss_function.gradient(&self.x, &self.y, predictions).mean().unwrap();
+            let gradient: f64 = self
+                .loss_function
+                .gradient(&self.x, &self.y, predictions)
+                .mean()
+                .unwrap();
             gradients.push(gradient);
         }
 
@@ -64,9 +83,11 @@ impl model::Modeller for Logistic {
     fn fit(&mut self, num_epochs: u32, learning_rate: f64) -> Result<(), PolarsError> {
         // Check if data are valid
         if self.x.shape().0 != self.y.len() {
-            return Err(PolarsError::ShapeMismatch("Shape mismatch between X and y".into()));
+            return Err(PolarsError::ShapeMismatch(
+                "Shape mismatch between X and y".into(),
+            ));
         }
-        
+
         for _ in 0..num_epochs {
             let predictions: Series = self.predict(&self.x)?;
             let gradients: (f64, Vec<f64>) = self.compute_gradients(&predictions);
@@ -81,5 +102,37 @@ impl model::Modeller for Logistic {
         Ok(())
     }
 
-    
+    fn predict(&self, x: &DataFrame) -> Result<Series, PolarsError> {
+        let mut predictions: Series = Series::new("prediction", vec![self.intercept; x.height()]);
+
+        for (i, coef) in self.coefficients.iter().enumerate() {
+            let feature_values: &Series = &x.get_col_by_index(i).unwrap();
+            predictions = feature_values * *coef + predictions;
+        }
+
+        // Apply sigmoid function to get probabilities
+        let prediction_probabilities: ChunkedArray<Float64Type> = predictions.f64().unwrap().apply(|value| 
+            if value.is_nan() { 
+                None
+            } else {
+                Some(1.0 / (1.0 + (value.unwrap() * -1.0).exp()))
+            }
+        );
+        
+        Ok(prediction_probabilities.into_series())
+    }
+
+    fn accuracy(&self, x: &DataFrame, y: &Series) -> Result<f64, PolarsError> {
+        let y_pred: Series = self.predict(x)?;
+        let y_pred_rounded: Series = Logistic::round_predictions(&y_pred);
+        let correct_predictions: Series = y_pred_rounded.equal(y).unwrap().into_series();
+        let num_correct_predictions: f64 = correct_predictions.sum().unwrap();
+        let accuracy: f64 = num_correct_predictions / y_pred_rounded.len() as f64;
+        Ok(accuracy)
+    }
+
+    fn loss(&self, x: &DataFrame, y: &Series) -> Result<f64, PolarsError> {
+        let y_pred = self.predict(x)?;
+        Ok(self.loss_function.loss(y, &y_pred))
+    }
 }
