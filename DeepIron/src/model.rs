ec073a1acb43d51bf394bedd1450799a3330b9a9
Saumya Shah <68504323+LordSaumya@users.noreply.github.com>
2024-06-23 18:11:39 +0800

    Add derived attributes for activation and loss function enums

--- DeepIron/src/model.rs
+++ DeepIron/src/model.rs
@@ -124,7 +124,7 @@ pub mod loss_functions {
     use polars::{frame::DataFrame, series::Series};
 
     /// Enum of supported loss functions.
-    #[derive(Clone)]
+    #[derive(Clone, PartialEq)]
     pub enum LossFunctionType {
         /// Mean squared error loss function.
         MeanSquaredError,
@@ -399,7 +399,7 @@ pub mod activation_functions {
     use polars::series::Series;
 
     /// Enum of supported activation functions.
-    #[derive(Clone)]
+    #[derive(Clone, PartialEq, Debug)]
     pub enum ActivationFunctionType {
         /// Identity activation function (does nothing)
         Identity,
